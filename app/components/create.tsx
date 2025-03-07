a338bcd8b6a629a61ac8037be3533f2ddcd59f54
nashmonzon <ignmonzon@gmail.com>
2025-03-07 20:10:28 +0100

    added tldraw

---
+++ app/components/create.tsx
@@ -0,0 +1,84 @@
+import {
+  Geometry2d,
+  HTMLContainer,
+  RecordProps,
+  Rectangle2d,
+  ShapeUtil,
+  T,
+  TLBaseShape,
+  TLResizeInfo,
+  Tldraw,
+  resizeBox,
+} from "tldraw";
+import "tldraw/tldraw.css";
+
+// There's a guide at the bottom of this file!
+
+// [1]
+type ICustomShape = TLBaseShape<
+  "my-custom-shape",
+  {
+    w: number;
+    h: number;
+    text: string;
+  }
+>;
+
+// [2]
+export class MyShapeUtil extends ShapeUtil<ICustomShape> {
+  // [a]
+  static override type = "my-custom-shape" as const;
+  static override props: RecordProps<ICustomShape> = {
+    w: T.number,
+    h: T.number,
+    text: T.string,
+  };
+
+  // [b]
+  getDefaultProps(): ICustomShape["props"] {
+    return {
+      w: 200,
+      h: 200,
+      text: "I'm a shape!",
+    };
+  }
+
+  // [c]
+  override canEdit() {
+    return false;
+  }
+  override canResize() {
+    return true;
+  }
+  override isAspectRatioLocked() {
+    return false;
+  }
+
+  // [d]
+  getGeometry(shape: ICustomShape): Geometry2d {
+    return new Rectangle2d({
+      width: shape.props.w,
+      height: shape.props.h,
+      isFilled: true,
+    });
+  }
+
+  // [e]
+  override onResize(shape: any, info: TLResizeInfo<any>) {
+    return resizeBox(shape, info);
+  }
+
+  // [f]
+  component(shape: ICustomShape) {
+    return (
+      <HTMLContainer style={{ backgroundColor: "#efefef" }}>
+        {shape.props.text}
+      </HTMLContainer>
+    );
+  }
+
+  // [g]
+  indicator(shape: ICustomShape) {
+    return <rect width={shape.props.w} height={shape.props.h} />;
+  }
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,11 +2,13 @@ import { Button } from "@/components/ui/button";
 
 import { ModeToggle } from "./components/mode-toggle";
 import { HydrateClient } from "./trpc/server";
+import { DrawEditor } from "./components/draw-editor";
 
 export default async function Home() {
   return (
     <HydrateClient>
       <main className="flex flex-col h-screen w-full overflow-hidden">
+        <DrawEditor />
         <Button>Click me</Button>
         <div className="fixed bottom-4 right-4">
           <ModeToggle />
